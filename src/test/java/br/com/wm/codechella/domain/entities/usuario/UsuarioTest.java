59c1b75aa80e0e1ac34db961db24096db26c6405
William Mian <william_mian@yahoo.com.br>
2024-07-16 13:28:31 -0300

    enriquecendo o dominio

---
+++ src/test/java/br/com/wm/codechella/domain/entities/usuario/UsuarioTest.java
@@ -0,0 +1,51 @@
+package br.com.wm.codechella.domain.entities.usuario;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class UsuarioTest {
+
+    @Test
+    public void naoDeveCadastrarUsuarioComMenosDe18anos() {
+
+        // Crio uma data de nascimento que subtrai 17 anos da data atual
+        LocalDate dataNascimento = LocalDate.now().minusYears(17);  // Um usuário com 17 anos
+
+        // Crio uma exceção e atribuo a ela o resultado do teste de idade mínima, pois o correto é que a exceção seja lançada.
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Usuario("123.456.789-00", "Fulano", dataNascimento, "fulano@example.com");
+        });
+
+        // Confiro se a mensagem da exceção é a mensagem que eu esperava, referente à idade inferior
+        Assertions.assertEquals("Usuário deve ter pelo menos 18 anos de idade!", exception.getMessage());
+    }
+
+    @Test
+    public void naoDeveCadastrarUsuarioComCpfNoFormatoInvalido(){
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("123456.789-99", "Jacque", LocalDate.parse("1990-09-08"), "email@email.com"));
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("12345678999", "Jacque", LocalDate.parse("1990-09-08"), "email@email.com"));
+
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("", "Jacque", LocalDate.parse("1990-09-08"), "email@email.com"));
+    }
+
+    @Test
+    public void deveCriarUsuarioUsandoFabricaDeUsuario(){
+        FabricaDeUsuario fabrica = new FabricaDeUsuario();
+        Usuario usuario = fabrica.comNomeCpfNascimento("Emily", "654.123.897-88",
+                LocalDate.parse("2000-10-01"));
+
+        Assertions.assertEquals("Emily", usuario.getNome());
+
+        usuario = fabrica.incluiEndereco("12345-999", 40, "apto 201");
+
+        Assertions.assertEquals("apto 201", usuario.getEndereco().getComplemento());
+    }
+
+}
